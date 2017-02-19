8300f8e98ab581a793c72d94c46f0506a0f89e6c
Nick Ivanov <nnrowan@gmail.com>
2017-02-19 20:15:22 +0200

    Save Button added to the toolbar

--- src/nsc/MainToolBar.java
+++ src/nsc/MainToolBar.java
@@ -2,12 +2,17 @@ package nsc;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class MainToolBar extends ToolBar {
     private Button saveButton;
 
     public MainToolBar() {
-        saveButton = new Button("\uD83D\uDCBE");
+
+        ImageView saveButtonImage = new ImageView(new Image("nsc/images/save_button.png"));
+        saveButton = new Button();
+        saveButton.setGraphic(saveButtonImage);
         this.getItems().add(saveButton);
     }
 }
