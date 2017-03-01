595e0e85b47b2ac1c2f7f10b73e3745f85fb98ee
Nick Ivanov <nnrowan@gmail.com>
2017-03-01 20:12:12 +0200

    Print button added

--- src/nsc/MainToolBar.java
+++ src/nsc/MainToolBar.java
@@ -8,6 +8,7 @@ import javafx.scene.image.ImageView;
 public class MainToolBar extends ToolBar {
     private Button saveButton;
     private Button clearButton;
+    private Button printButton;
 
     public MainToolBar() {
 
@@ -15,13 +16,18 @@ public class MainToolBar extends ToolBar {
         saveButton = new Button();
         saveButton.setGraphic(saveButtonImage);
 
+        ImageView printButtonImage = new ImageView(new Image("nsc/images/print_button.png"));
+        printButton = new Button();
+        printButton.setGraphic(printButtonImage);
+
         ImageView clearButtonImage = new ImageView(new Image("nsc/images/clear_button.png"));
         clearButton = new Button();
         clearButton.setGraphic(clearButtonImage);
 
 
+
         BitBox bitBox = new BitBox();
 
-        this.getItems().addAll(saveButton, clearButton, bitBox);
+        this.getItems().addAll(saveButton, printButton, clearButton, bitBox);
     }
 }
