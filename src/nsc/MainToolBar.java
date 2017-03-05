e17d93b5a6d187c04480950f3fca7ccd1bee7c07
Nick Ivanov <nnrowan@gmail.com>
2017-03-05 20:15:10 +0200

    Additional commit

--- src/nsc/MainToolBar.java
+++ src/nsc/MainToolBar.java
@@ -15,8 +15,13 @@ public class MainToolBar extends ToolBar {
     private Button saveButton;
     private Button clearButton;
     private Button printButton;
+    private Button openButton;
 
     public MainToolBar(Stage stage) {
+
+        ImageView openButtonImage = new ImageView(new Image("nsc/images/open_button.png"));
+        openButton = new Button();
+        openButton.setGraphic(openButtonImage);
 
         ImageView saveButtonImage = new ImageView(new Image("nsc/images/save_button.png"));
         saveButton = new Button();
@@ -29,6 +34,20 @@ public class MainToolBar extends ToolBar {
         ImageView clearButtonImage = new ImageView(new Image("nsc/images/clear_button.png"));
         clearButton = new Button();
         clearButton.setGraphic(clearButtonImage);
+
+        openButton.setOnAction(event -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Open Resource File");
+            fileChooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                    new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
+                    new FileChooser.ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"),
+                    new FileChooser.ExtensionFilter("All Files", "*.*"));
+            File selectedFile = fileChooser.showOpenDialog(stage);
+            if (selectedFile != null) {
+                System.out.println("Length: " + selectedFile.length());
+            }
+        });
 
         saveButton.setOnAction(event -> {
             FileChooser fileChooser = new FileChooser();
@@ -49,6 +68,6 @@ public class MainToolBar extends ToolBar {
 
         BitBox bitBox = new BitBox();
 
-        this.getItems().addAll(saveButton, printButton, clearButton, bitBox);
+        this.getItems().addAll(openButton, saveButton, printButton, clearButton, bitBox);
     }
 }
