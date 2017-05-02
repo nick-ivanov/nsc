37fe0ae69692211c961ecdee1884228d81456006
Nick Ivanov <nnrowan@gmail.com>
2017-05-02 19:31:12 +0200

    Removing isLegitFile() from MainToolBar class and code cleanup

--- src/nsc/MainToolBar.java
+++ src/nsc/MainToolBar.java
@@ -8,31 +8,23 @@ import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class MainToolBar extends ToolBar {
-    private Button saveButton;
-    private Button clearButton;
-    private Button printButton;
-    private Button openButton;
     private BitBox bitBox;
 
-    private CenterContainer centerContainer;
-
     public MainToolBar(Stage stage, CenterContainer centerContainer) {
-        this.centerContainer = centerContainer;
-
         ImageView openButtonImage = new ImageView(new Image("nsc/images/open_button.png"));
-        openButton = new Button();
+        Button openButton = new Button();
         openButton.setGraphic(openButtonImage);
 
         ImageView saveButtonImage = new ImageView(new Image("nsc/images/save_button.png"));
-        saveButton = new Button();
+        Button saveButton = new Button();
         saveButton.setGraphic(saveButtonImage);
 
         ImageView printButtonImage = new ImageView(new Image("nsc/images/print_button.png"));
-        printButton = new Button();
+        Button printButton = new Button();
         printButton.setGraphic(printButtonImage);
 
         ImageView clearButtonImage = new ImageView(new Image("nsc/images/clear_button.png"));
-        clearButton = new Button();
+        Button clearButton = new Button();
         clearButton.setGraphic(clearButtonImage);
 
         bitBox = new BitBox(centerContainer);
@@ -66,21 +58,5 @@ public class MainToolBar extends ToolBar {
 
     public BitBox getBitBox() {
         return bitBox;
-    }
-
-    private boolean isLegitFile(String msg) {
-        for(char ch : msg.toCharArray()) {
-            if(ch != '1' && ch != '0') {
-                return false;
-            }
-        }
-
-        int messageFieldMaxLength = Integer.parseInt(NSCPropertyHelper.getProperty("message_field_max_length"));
-
-        if (msg.length() > messageFieldMaxLength) {
-            return false;
-        }
-
-        return true;
     }
 }
