808654a857dfc248a0c70e7a34bb25994893ebd9
Nick Ivanov <nnrowan@gmail.com>
2017-05-04 21:15:13 +0200

    Get rid of redundant functions in PropertyHelper

--- src/nsc/NSCPropertyHelper.java
+++ src/nsc/NSCPropertyHelper.java
@@ -2,7 +2,6 @@ package nsc;
 
 import java.util.*;
 import java.io.*;
-import java.util.Map.Entry;
 
 public class NSCPropertyHelper {
     final static String PROPERTY_FILEPATH = "src/nsc/application.properties";
@@ -14,34 +13,6 @@ public class NSCPropertyHelper {
             Properties propertiesObj = new Properties();
             propertiesObj.load(reader);
             return propertiesObj.getProperty(property);
-        } catch (Exception ex) {
-            System.err.println("FATAL ERROR: " + ex.getMessage());
-            System.exit(ERROR_EXIT_CODE);
-        }
-
-        return null;
-    }
-
-    static String getPropertyWithSpaces(String property, String delimiter) {
-        try {
-            FileReader reader = new FileReader(PROPERTY_FILEPATH);
-            Properties propertiesObj = new Properties();
-            propertiesObj.load(reader);
-            return propertiesObj.getProperty(property).replaceAll(delimiter, "");
-        } catch (Exception ex) {
-            System.err.println("FATAL ERROR: " + ex.getMessage());
-            System.exit(ERROR_EXIT_CODE);
-        }
-
-        return null;
-    }
-
-    static Set<Entry<Object,Object>> getPropertySet() {
-        try {
-            FileReader reader = new FileReader(PROPERTY_FILEPATH);
-            Properties propertiesObj = new Properties();
-            propertiesObj.load(reader);
-            return propertiesObj.entrySet();
         } catch (Exception ex) {
             System.err.println("FATAL ERROR: " + ex.getMessage());
             System.exit(ERROR_EXIT_CODE);
